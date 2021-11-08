998ad69a8bfad8c16022746eb0943668f80e59ed
KevinVinsen <kvn1961@gmail.com>
2021-11-08 12:40:52 +0800

    Improved layout

--- tests/test_01.py
+++ tests/test_01.py
@@ -19,7 +19,7 @@
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 #    MA 02111-1307  USA
 #
-from yaml_to_kwargs import read_yaml
+from common_kv.yaml_to_kwargs import read_yaml
 
 
 def test_01():
